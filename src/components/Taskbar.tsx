7f99dfb0cf2a7318ec925eaad447610f562a54a3
Joaquin de la Iglesia <joaquindlaiglesia@gmail.com>
2024-10-02 16:00:54 -0300

    second commit yay generic!

---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Taskbar: React.FC = () => {
+  return (
+    <div className="bg-[#c1bbbb] fixed bottom-0 left-0 w-full text-black border-t-red-500">
+      Taskbar
+    </div>
+  );
+};
+
+export default Taskbar;
